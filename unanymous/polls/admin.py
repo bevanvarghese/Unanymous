ec850e45b1ea756f2dfd934909163d858ab66b25
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-07 20:40:02 +0530

    completed admin area

--- unanymous/polls/admin.py
+++ unanymous/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = 'Unanymous Admin'
+admin.site.site_title = 'Unanymous Admin Area'
+admin.site.index_title = 'Welcome to the Unanymous Admin Area'
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['published_on'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
