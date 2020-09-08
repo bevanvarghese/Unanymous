8b0dfeda650b7b119483c12189ef7757b59cd703
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-08 08:20:04 +0530

    completed pages

--- unanymous/polls/views.py
+++ unanymous/polls/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Question, Choice
+
+# Get and display questions
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-published_on')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Show question and choices
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+# Get and display results of question
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# Vote for a question choice
+
+
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Chhoice.DoesNotExist):
+        # Display the voting form again
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return a HttpResponseRedirect after dealing with POST requests
+        # to prevent data from being posted twice in the case of 'Back' pressed
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
