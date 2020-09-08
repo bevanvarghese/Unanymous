3f0309e21ca50905872fbc6214a6c5cb826ed513
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-08 20:45:23 +0530

    added screenshots

--- unanymous/polls/views.py
+++ unanymous/polls/views.py
@@ -6,16 +6,12 @@ from django.urls import reverse
 from .models import Question, Choice
 
 # Get and display questions
-
-
 def index(request):
     latest_question_list = Question.objects.order_by('-published_on')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 # Show question and choices
-
-
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
@@ -24,15 +20,11 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 # Get and display results of question
-
-
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 # Vote for a question choice
-
-
 def vote(request, question_id):
     # print(request.POST['choice'])
     question = get_object_or_404(Question, pk=question_id)
@@ -52,8 +44,6 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 # Data of a single question - API endpoint for graphing
-
-
 def resultsData(request, obj):
     voteData = []
     question = Question.objects.get(id=obj)
